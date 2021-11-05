526760eccc3a3c918c04d1ea7dfde9948270deda
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-06 06:23:33 +0700

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -15,6 +15,7 @@ from userbot import BOTLOG, BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, bot
 from userbot.events import man_cmd
+from userbot.utils import edit_or_reply
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 heroku_api = "https://api.heroku.com"
