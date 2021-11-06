ce5317da0fd28948a5dee7e855844c1b6c579deb
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-06 10:34:12 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -429,9 +429,11 @@ def paginate_help(page_number, loaded_modules, prefix):
     return pairs
 
 try:
-    from userbot.modules.sql_helper.globals import gvarstatus
-with bot:
-    try:
+    from userbot.modules.sql_helper.globals import delgvar, gvarstatus
+
+    bottoken = gvarstatus("BOT_TOKEN")
+    with bot:
+        try:
         tgbot = TelegramClient(
             "TG_BOT_TOKEN",
             api_id=API_KEY,
@@ -439,7 +441,7 @@ with bot:
             connection=ConnectionTcpAbridged,
             auto_reconnect=True,
             connection_retries=None,
-        ).start(bot_token=gvarstatus("BOT_TOKEN"))
+        ).start(bot_token=bottoken)
 
         dugmeler = CMD_HELP
         user = bot.get_me()
